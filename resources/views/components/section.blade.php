2286752e05c1e907b3ad144d5121e948d4b7a17e
Dan Dodd <dan@foredev.com>
2023-03-22 11:39:49 -0500

    better styles on staff edit

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,4 +1,4 @@
-<section "bg-gray-300 p-4 mb-6">
+<section class="bg-gray-300 p-4 mb-6">
     <div class="container">
         {{  $slot }}
     </div>
--- resources/views/staff/edit.blade.php
+++ resources/views/staff/edit.blade.php
@@ -4,36 +4,39 @@
             method="PATCH"
             action="/staff/{{ $staff->id }}"
         >
-
-            <div class="">
-                <label class="mb-2 uppercase font-bold text-xs text-gray-700" for="firstName"> First Name </label>
-                <x-input class="border border-gray-400 p-2 w-half"
-                          name="firstName" id="firstName"
-                          value="{{ $staff->firstName }}">
-                    {{ $staff->firstName }}
-                </x-input>
-
-                <label class="mb-2 uppercase font-bold text-xs text-gray-700" for="lastName"> Last Name </label>
-                <x-input class="border border-gray-400 p-2 w-half"
-                          name="lastName" id="lastName"
-                          value="{{ $staff->lastName }}">
-                    {{ $staff->lastName }}
-                </x-input>
-
-
-                <label class="mb-2 uppercase font-bold text-xs text-gray-700" for="designation"> Designation </label>
-                <x-input class="border border-gray-400 p-2 w-half"
-                        name="designation" id="designation"
-                        value="{{ $staff->designation }}">
-                    {{ $staff->designation }}
-                </x-input>
-                {{-- <label class="mb-2 uppercase font-bold text-xs text-gray-700" for="email"> Email </label>
-                <x-input class="border border-gray-400 p-2 w-fhalf"
-                          name="email" id="email"
-                          value="{{ $staff->email }}">
-                    {{ $staff->email }}
-                </x-input> --}}
-
+            <div class="flex flex-col">
+                <div class="p-2">
+                    <label class="p-6 mb-2 uppercase font-bold text-xs text-gray-700" for="firstName"> First Name </label>
+                    <x-input class="border border-gray-400 p-1.5 m-2"
+                            name="firstName" id="firstName"
+                            value="{{ $staff->firstName }}">
+                        {{ $staff->firstName }}
+                    </x-input>
+                </div>
+                <div class="p-2">
+                    <label class=" p-6 mb-2 uppercase font-bold text-xs text-gray-700" for="lastName"> Last Name </label>
+                    <x-input class=" border border-gray-400 p-1.5 m-2"
+                            name="lastName" id="lastName"
+                            value="{{ $staff->lastName }}">
+                        {{ $staff->lastName }}
+                    </x-input>
+                </div>
+                <div class="p-2">
+                    <label class=" p-6 mb-2 uppercase font-bold text-xs text-gray-700" for="designation"> Designation </label>
+                    <x-input class="border border-gray-400 p-1.5"
+                            name="designation" id="designation"
+                            value="{{ $staff->designation }}">
+                        {{ $staff->designation }}
+                    </x-input>
+                </div>
+                {{-- <div class="basis-1/4" >
+                    <label class="mb-2 uppercase font-bold text-xs text-gray-700" for="email"> Email </label>
+                    <x-input class="border border-gray-400 p-2 w-fhalf"
+                            name="email" id="email"
+                            value="{{ $staff->email }}">
+                        {{ $staff->email }}
+                    </x-input> --}}
+                </div>
 
             </div>
 
